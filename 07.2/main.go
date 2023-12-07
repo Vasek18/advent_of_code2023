b04354f44863e436fc8fd7338373bd6c3730e0be
aristovvasiliy <vasiliy.aristov@supermetrics.com>
2023-12-07 12:25:52 +0200

    07.2

---
+++ 07.2/main.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Hand struct {
+	hand                string
+	combinationStrength float32
+	// cardsStrength       int
+	formattedCards string
+	bid            int
+}
+
+func solve(hands []Hand) int {
+	sum := 0
+
+	sort.Slice(hands, func(i, j int) bool {
+		hand1 := hands[i]
+		hand2 := hands[j]
+
+		if hand1.combinationStrength > hand2.combinationStrength {
+			return false
+		}
+		if hand1.combinationStrength < hand2.combinationStrength {
+			return true
+		}
+		if hand1.formattedCards > hand2.formattedCards {
+			return false
+		}
+		if hand1.formattedCards < hand2.formattedCards {
+			return true
+		}
+
+		return false
+	})
+
+	// fmt.Println(hands)
+
+	for i, hand := range hands {
+		fmt.Println((i + 1), hand.bid, hand.hand, hand.combinationStrength)
+		sum += (i + 1) * hand.bid
+	}
+
+	return sum
+}
+
+func getCombinationStrength(hand string) float32 {
+	charCount := make(map[rune]int)
+
+	numberOfJacks := 0
+	for _, char := range hand {
+		charCount[char]++
+
+		if char == 'J' {
+			numberOfJacks++
+		}
+	}
+
+	numberOfPairs := 0
+	numberOfThrees := 0
+	for _, count := range charCount {
+		if count == 5 {
+			return 5
+		}
+
+		if count == 4 {
+			if numberOfJacks > 0 {
+				return 5
+			}
+
+			return 4
+		}
+
+		if count == 2 {
+			numberOfPairs++
+		}
+		if count == 3 {
+			numberOfThrees++
+		}
+	}
+
+	if numberOfPairs == 1 && numberOfThrees == 1 {
+		if numberOfJacks > 0 {
+			return 5
+		}
+
+		return 3.5
+	}
+
+	if numberOfThrees == 1 {
+		if numberOfJacks > 0 {
+			return 4
+		}
+
+		return 3
+	}
+
+	if numberOfPairs == 2 {
+		if numberOfJacks == 1 {
+			return 3.5
+		}
+		if numberOfJacks == 2 {
+			return 4
+		}
+
+		return 2.5
+	}
+
+	if numberOfPairs == 1 {
+		if numberOfJacks > 0 {
+			return 3
+		}
+
+		return 2
+	}
+
+	if numberOfJacks == 1 {
+		return 2
+	}
+
+	return 1
+}
+
+// func getCardsStrength(hand string) int {
+// 	strength := 0
+
+// 	for i, card := range hand {
+// 		multiplier := int(math.Pow(10, float64(5-i)))
+
+// 		if string(card) == "A" {
+// 			strength += 14 * multiplier
+// 			continue
+// 		}
+// 		if string(card) == "K" {
+// 			strength += 13 * multiplier
+// 			continue
+// 		}
+// 		if string(card) == "Q" {
+// 			strength += 12 * multiplier
+// 			continue
+// 		}
+// 		if string(card) == "J" {
+// 			strength += 11 * multiplier
+// 			continue
+// 		}
+// 		if string(card) == "T" {
+// 			strength += 10 * multiplier
+// 			continue
+// 		}
+
+// 		strength += convertToInt(string(card)) * multiplier
+// 	}
+
+// 	return strength
+// }
+
+func formatCardsForSorting(hand string) string {
+	formattedString := ""
+
+	for _, card := range hand {
+		if string(card) == "A" {
+			formattedString += "Z"
+			continue
+		}
+		if string(card) == "K" {
+			formattedString += "Y"
+			continue
+		}
+		if string(card) == "Q" {
+			formattedString += "X"
+			continue
+		}
+		if string(card) == "J" {
+			formattedString += "1"
+			continue
+		}
+		if string(card) == "T" {
+			formattedString += "V"
+			continue
+		}
+
+		formattedString += string(card)
+	}
+
+	return formattedString
+}
+
+func prepareHands(inputStrings []string) []Hand {
+	var hands []Hand
+
+	for _, rawHand := range inputStrings {
+
+		rawHandParts := strings.Fields(rawHand)
+		hand := Hand{
+			hand:                rawHandParts[0],
+			combinationStrength: getCombinationStrength(rawHandParts[0]),
+			// cardsStrength:       getCardsStrength(rawHandParts[0]),
+			formattedCards: formatCardsForSorting(rawHandParts[0]),
+			bid:            convertToInt(rawHandParts[1]),
+		}
+
+		hands = append(hands, hand)
+	}
+
+	return hands
+}
+
+func main() {
+	input, _ := os.ReadFile("./07.2/input.txt")
+
+	hands := prepareHands(strings.Split(string(input), "\n"))
+
+	totalSum := solve(hands)
+
+	fmt.Println(totalSum)
+}
+
+func convertToInt(s string) int {
+	n, _ := strconv.Atoi(s)
+	return n
+}
