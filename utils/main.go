bf45a3c03877803d876806ce0061e42e381e0d5e
Vasiliy Aristov <v.aristov@sgmsoft.com>
2023-12-10 12:08:39 +0200

    10.1

--- utils/main.go
+++ utils/main.go
@@ -8,3 +8,19 @@ func ConvertStringToInt(s string) int {
 	n, _ := strconv.Atoi(s)
 	return n
 }
+
+func Max(x, y int) int {
+	if x < y {
+		return y
+	}
+
+	return x
+}
+
+func Min(x, y int) int {
+	if x > y {
+		return y
+	}
+
+	return x
+}
