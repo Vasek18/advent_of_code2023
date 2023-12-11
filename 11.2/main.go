70b1060a4c6343cb976afbec1242572098dcf8b2
aristovvasiliy <vasiliy.aristov@supermetrics.com>
2023-12-11 15:36:41 +0200

    11.2

---
+++ 11.2/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strings"
+)
+
+type Galaxy struct {
+	n   int
+	lat int
+	lon int
+}
+
+func readMap(input string) ([]Galaxy, []int, []int) {
+	var ourMap []Galaxy
+
+	inputRows := strings.Split(input, "\n")
+
+	rowsMap := make([]bool, len(inputRows))
+	colummnsMap := make([]bool, len(inputRows[0]))
+
+	n := 0
+	for lon, r := range inputRows {
+		for lat, c := range strings.Split(r, "") {
+			if c == "#" {
+				n++
+				ourMap = append(ourMap, Galaxy{n, lat, lon})
+
+				rowsMap[lon] = true
+				colummnsMap[lat] = true
+			}
+		}
+	}
+
+	var emptyRows []int
+	for i, r := range rowsMap {
+		if r == false {
+			emptyRows = append(emptyRows, i)
+		}
+	}
+
+	var emptyColumns []int
+	for i, c := range colummnsMap {
+		if c == false {
+			emptyColumns = append(emptyColumns, i)
+		}
+	}
+
+	return ourMap, emptyRows, emptyColumns
+}
+
+func calcDistance(g1 Galaxy, g2 Galaxy, emptyRows []int, emptyColumns []int) int {
+	manhattanDistance := int(math.Abs(float64(g1.lat-g2.lat)) + math.Abs(float64(g1.lon-g2.lon)))
+
+	coef := 1000000 - 1
+	for _, r := range emptyRows {
+		if r > g1.lon && r < g2.lon {
+			manhattanDistance += coef
+		}
+		if r < g1.lon && r > g2.lon {
+			manhattanDistance += coef
+		}
+	}
+
+	for _, c := range emptyColumns {
+		if c > g1.lat && c < g2.lat {
+			manhattanDistance += coef
+		}
+		if c < g1.lat && c > g2.lat {
+			manhattanDistance += coef
+		}
+	}
+
+	return manhattanDistance
+}
+
+func solve(ourMap []Galaxy, emptyRows []int, emptyColumns []int) int {
+	answer := 0
+
+	for i := 0; i < len(ourMap)-1; i++ {
+		g1 := ourMap[i]
+		for j := i + 1; j < len(ourMap); j++ {
+			g2 := ourMap[j]
+
+			answer += calcDistance(g1, g2, emptyRows, emptyColumns)
+		}
+	}
+
+	return answer
+}
+
+func main() {
+	input, _ := os.ReadFile("./11.2/input.txt")
+	ourMap, emptyRows, emptyColumns := readMap(string(input))
+
+	answer := solve(ourMap, emptyRows, emptyColumns)
+
+	fmt.Println(answer)
+}
