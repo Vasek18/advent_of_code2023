7a5fbc2c48e6fbbe4387d2ff6bc516aa0206f858
aristovvasiliy <vasiliy.aristov@supermetrics.com>
2023-12-14 11:38:47 +0200

    14.1

--- 14.1/main.go
+++ 14.1/main.go
@@ -13,11 +13,20 @@ func readInput(input string) []string {
 func solve(rows []string) int {
 	answer := 0
 
-	multiplier := 1
-	for i := len(rows) - 1; i >= 0; i-- {
-		answer += strCount(rows[i], "O") * multiplier
-		fmt.Println(strCount(rows[i], "O"), multiplier)
-		multiplier++
+	for j := 0; j <= len(rows[0])-1; j++ {
+		multiplier := len(rows)
+		for i := 0; i <= len(rows)-1; i++ {
+			char := rows[i][j]
+
+			if char == 'O' {
+				answer += multiplier
+				multiplier--
+			}
+
+			if char == '#' {
+				multiplier = len(rows) - i - 1
+			}
+		}
 	}
 
 	return answer
